34f733ec9b6aa322ed15fd9055d54d7df58c37e5
cmungall <cjm@berkeleybop.org>
2019-07-28 12:45:44 -0700

    deps

--- install.pl
+++ install.pl
@@ -1,9 +1,17 @@
 
 install_requirements :-
+        writeln(loading),
         ensure_loaded(pack),
         Opts=[interactive(false)],
         requires(X),
+        writeln(installing(X)),
         pack_install(X, Opts),
         fail.
 install_requirements.
-    
+
+
+install_this :-
+        Opts=[interactive(false)],
+        install_requirements,
+        name(X),
+        pack_install(X,Opts).
